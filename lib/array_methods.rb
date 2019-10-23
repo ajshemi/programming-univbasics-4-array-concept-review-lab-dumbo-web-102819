cd14256fc66a745cfb99859c40ed3c3e5cac6a07
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-23 20:37:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,6 +11,18 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  i=0 
+  max=0
+  while i<array.length do 
+    if array[i]>max
+      max=array[i]
+    else
+      max 
+    end
+    i+=1
+  end
+  max
+      
   # Add your solution here
 end
 
