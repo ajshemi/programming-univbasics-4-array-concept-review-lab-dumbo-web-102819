4eaf6ddfc4a9953f2558ca536f29ddcf4197d2a8
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-23 20:41:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -27,5 +27,16 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
+  i=0 
+  min=0
+  while i<array.length do 
+    if array[i]<min
+      min=array[i]
+    else
+      min
+    end
+    i+=1
+  end
+  min
   # Add your solution here
 end
